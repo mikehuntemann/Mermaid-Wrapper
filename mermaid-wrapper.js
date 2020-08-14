456b0c6a6badd81f31e70dc183407d4959e2557f
Mike Huntemann <mail@mikehuntemann.de>
2020-08-14 16:43:03 +0200

    refactored content classification

--- mermaid-wrapper.js
+++ mermaid-wrapper.js
@@ -8,6 +8,7 @@
 const { readFileSync } = require('fs')
 const path = require('path')
 const crypto = require('crypto')
+const { isDeepStrictEqual } = require('util')
 
 const filename = 'bat-tauschverhaeltnisse.md'
 const filepath = path.resolve(`samples/${filename}`)
@@ -17,7 +18,7 @@ const lastContent = slices.length
 
 const isHeadline = (slice) => {
   if (slice.charAt(0) !== '#') {
-    return false
+    return
   }
   return true
 }
@@ -37,7 +38,7 @@ const hasRelation = (slice) => {
 }
 
 const isSuper = (slice) => {
-  if (slice.charAt(0) !== '*') {
+  if (slice.charAt(0) !== '*' || !hasRelation(slice)) {
     return false
   }
   return true
@@ -74,8 +75,20 @@ const componentToObject = (comp) => {
     }
   }
 }
+const removeFormatting = (type, string) => {
+  if (!string || !type) {
+    return
+  }
+  if (type === contentTypes.HEADLINE) {
+    return removeHeadlineFormatting(string)
+  }
+  if (type === contentTypes.SUPER) {
+    return removeSuperFormatting(string)
+  }
+  return string
+}
 
-const removeBoldFormatting = (string) => {
+const removeSuperFormatting = (string) => {
   const re = /(\*\*|__)(.*?)\1/
   return string.match(re)[2]
 }
@@ -84,38 +97,98 @@ const removeHeadlineFormatting = (string) => {
   return string.replace('## ', '')
 }
 
-const getContentId = (string) => {
+const getContentID = (string, mode = 'UNIQUE') => {
+  if (!string) {
+    return
+  }
+  const contentID = crypto
+    .createHash('sha1')
+    .update(string)
+    .digest('hex')
+    .slice(0, 6)
+
+  if (mode !== 'UNIQUE') {
+    return contentID
+  }
   // stackoverflow.com/questions/5878682/node-js-hash-string#comment15839272_11869589
-  return crypto.createHash('sha1').update(string).digest('hex').slice(0, 6)
+  return makeContentIDUnique(contentID)
 }
 
-const getUniqueContentID = (string) => {
+const makeContentIDUnique = (base) => {
   const randomString = crypto.randomBytes(1).toString('hex')
-  const base = getContentId(string)
   return `${base}/${randomString}`
 }
 
 const getStartRange = (array, currentIndex) => {
+  if (!array || !currentIndex) {
+    return
+  }
   return array[currentIndex].meta.line
 }
 
 const getEndRange = (array, currentIndex) => {
+  if (!array || !currentIndex) {
+    return
+  }
   const nextIndex = currentIndex + 1
-  console.log(
-    'currentIndex:',
-    currentIndex,
-    'nextIndex:',
-    nextIndex,
-    'array.length:',
-    array.length
-  )
   if (array.length <= nextIndex) {
     return lastContent
   }
   return array[nextIndex].meta.line - 1
 }
 
+let lastHeadline
+let lastHeadlineId
+let lastSuper
+let lastSuperId
+
+const setObjHistory = (type, label, id) => {
+  if (!type || !label || !id) {
+    return
+  }
+  if (type === contentTypes.HEADLINE) {
+    lastHeadline = label
+    lastHeadlineId = id
+  }
+  if (type === contentTypes.SUPER) {
+    lastSuper = label
+    lastSuperId = id
+  }
+}
+
+
+const addContextToObject = (obj) => {
+  const context = {
+    context: {
+      label: lastHeadline,
+      id: lastHeadlineId
+    }
+  }
+  obj.meta = context
+  console.log(obj)
+  return obj
+}
+
+const createObjectFromString = (type, string, line, mode = 'UNIQUE') => {
+  const label = removeFormatting(type, string)
+  const id = getContentID(label, mode)
+  const obj = {
+    label,
+    id,
+    meta: {
+      line,
+      type,
+      mode
+    }
+  }
+  setObjHistory(type, label, id)
+  return obj
+}
+
 const getCandidates = (collection, type, startRange, endRange) => {
+  if (!collection || !type || !startRange || !endRange) {
+    return
+  }
   const candidaties = collection.filter(
     (entry) =>
       entry.meta.type === type &&
@@ -126,75 +199,82 @@ const getCandidates = (collection, type, startRange, endRange) => {
 }
 
 const getTypeMatch = (collection, type) => {
+  if (!collection || !type) {
+    return
+  }
   return collection.filter((entry) => entry.meta.type === type)
 }
 
 const structureCollection = []
 const relationsCollection = []
 
-let lastHeadline
-let lastHeadlineId
-let lastSuper
-let lastSuperId
+const contentTypes = {
+  COMMENT: 'COMMENT',
+  HEADLINE: 'HEADLINE',
+  SUPER: 'SUPER',
+  DETAIL: 'DETAIL'
+}
+
+const getContentType = (string) => {
+  if (isComment(string)) {
+    return contentTypes.COMMENT
+  }
+  else if (isHeadline(string)) {
+    return contentTypes.HEADLINE
+  }
+  else if (isSuper(string)) {
+    return contentTypes.SUPER
+  }
+  else if (hasRelation(string)) {
+    return contentTypes.DETAIL
+  }
+  else {
+    return undefined
+  }
+}
 
 const classifyContent = (content) => {
-  if (hasMeta(content)) {
-    console.log('hasMeta: true')
-    // ToDo: read header, overwrite defaults
-  } else {
+  if (!hasMeta(content)) {
     console.log('hasMeta: false')
     // ToDo: no header, use defaults
   }
+  console.log('hasMeta: true')
+  const mode = 'UNIQUE'
+  // ToDo: read header, overwrite defaults
   content.forEach((slice, index) => {
-    if (isComment(slice)) {
+    const type = getContentType(slice)
+    // console.log(type)
+    if (!type) {
       return
     }
-    if (isHeadline(slice)) {
-      lastHeadline = removeHeadlineFormatting(slice)
-      lastHeadlineId = getUniqueContentID(lastHeadline)
-      const sliceObj = {
-        id: lastHeadlineId,
-        label: lastHeadline,
-        meta: {
-          line: index,
-          type: 'headline',
-          mode: 'unique' // ToDo
-        }
+    const obj = createObjectFromString(type, slice, index, mode)
+    switch (type) {
+      case contentTypes.HEADLINE: structureCollection.push(obj)
+      case contentTypes.SUPER: {
+        // ERR: does not add infos to object
+        const superObj = addContextToObject(obj)
+        structureCollection.push(superObj)
       }
-      structureCollection.push(sliceObj)
-    }
-    if (isSuper(slice)) {
-      // supers should not be unique? > handle current case of doube entries with different lines
-      lastSuper = removeBoldFormatting(slice)
-      lastSuperId = getUniqueContentID(lastSuper)
-      const sliceObj = {
-        id: lastSuperId,
-        label: lastSuper,
-        meta: {
-          context: {
-            label: lastHeadline,
-            id: lastHeadlineId
-          },
-          line: index,
-          type: 'super',
-          mode: 'unique' // ToDo
-        }
+      case contentTypes.DETAIL: {
+        // addComponentsToObject function missing
+        relationsCollection.push(obj)
       }
-      structureCollection.push(sliceObj)
     }
+    /*
     if (hasRelation(slice) && !isSuper(slice)) {
       const label = slice
       const comps = splitComponents(slice)
       const sliceObj = componentToObject(comps)
       sliceObj.label = label
-      sliceObj.id = getUniqueContentID(label)
-      sliceObj.content.sender.id = getUniqueContentID(
+      // ToDo: add mode to getContentID
+      sliceObj.id = getContentID(label)
+      sliceObj.content.sender.id = getContentID(
         sliceObj.content.sender.label
       )
-      sliceObj.content.object.id = getUniqueContentID(
+      sliceObj.content.object.id = getContentID(
         sliceObj.content.object.label
       )
-      sliceObj.content.receiver.id = getUniqueContentID(
+      sliceObj.content.receiver.id = getContentID(
         sliceObj.content.receiver.label
       )
       sliceObj.meta = {
@@ -207,14 +287,18 @@ const classifyContent = (content) => {
         mode: 'unique' // ToDo
       }
       relationsCollection.push(sliceObj)
-    }
+    }*/
   })
+  console.log(structureCollection[0])
 }
 
 const createBlockIndex = () => {
   // HEADLINES WITHIN DOCUMENT
-  const headlines = getTypeMatch(structureCollection, 'headline')
-  console.log(headlines)
+  const headlines = getTypeMatch(structureCollection, contentTypes.HEADLINE)
+  // console.log(headlines)
+  if (!headlines) {
+    return
+  }
   for (let i = 0; i < headlines.length; i++) {
     const headlineStartRange = getStartRange(headlines, i)
     const headlineEndRange = getEndRange(headlines, i)
@@ -222,23 +306,29 @@ const createBlockIndex = () => {
     // SUPERS WITHIN EACH HEADLINE
     const supers = getCandidates(
       structureCollection,
-      'super',
+      contentTypes.SUPER,
       headlineStartRange,
       headlineEndRange
     )
-    console.log(supers)
+    // console.log(supers)
 
     // DETAILS WITHIN EACH SUPER
+    if (!supers) {
+      return
+    }
     for (let i = 0; i < supers.length; i++) {
       const superStartRange = getStartRange(supers, i)
       const superEndRange = getEndRange(supers, i)
       const details = getCandidates(
         relationsCollection,
-        'detail',
+        contentTypes.DETAIL,
         superStartRange,
         superEndRange
       )
-      console.log('details:', details)
+      if (!details) {
+        return
+      }
+      // console.log('details:', details)
     }
     // ToDo: Save IDs of BlockCandidates (with ID of SUPER)
   }
